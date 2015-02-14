840f9ec94987fb5acef05c0cb934d87bdab25918
Ubuntu <ubuntu@ip-172-31-10-30.eu-central-1.compute.internal>
2015-02-14 20:35:33 +0000

    added some modifications on production server

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('tunte-MINT3'),
+	'local' => array('tunte-MINT3','ip-172-31-10-30'),
 
 ));
 
